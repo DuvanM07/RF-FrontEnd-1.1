4de6698f928da81ae709bf9334971023a9d593af
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-03-04 15:35:16 -0500

    [+] ng g c pages/private/categories --skip-tests [+] ng g c pages/private/categories/category-register --skip-tests

---
+++ src/app/pages/private/categories/category-register/category-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-register',
+  imports: [],
+  templateUrl: './category-register.component.html',
+  styleUrl: './category-register.component.css'
+})
+export class CategoryRegisterComponent {
+
+}

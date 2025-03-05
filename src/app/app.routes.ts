03339950c12ea581968b8507d39b7032b246c6d5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2025-03-05 12:45:11 -0500

    [+] product.component: Implementa ReactiveForms, DefineValidacion en c/u campos y obtiene los datos e Implementa despliege de mensajes de error por cada campo de formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,8 @@ import { UsersComponent } from './pages/private/users/users.component';
 import { UserRegisterComponent } from './pages/private/users/user-register/user-register.component';
 import { CategoriesComponent } from './pages/private/categories/categories.component';
 import { CategoryRegisterComponent } from './pages/private/categories/category-register/category-register.component';
+import { ProductsComponent } from './pages/private/products/products.component';
+import { ProductRegisterComponent } from './pages/private/products/product-register/product-register.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
@@ -18,8 +20,10 @@ export const routes: Routes = [
     { path: 'dashboard', component: DashboardComponent },
     { path: '404', component: PageNotFoundComponent },
     { path: 'dashboard/users', component: UsersComponent },
+    { path: 'dashboard/products', component: ProductsComponent },
     { path: 'dashboard/categories', component: CategoriesComponent },
     { path: 'dashboard/user/new', component: UserRegisterComponent },
+    { path: 'dashboard/product/new', component: ProductRegisterComponent },
     { path: 'dashboard/category/new', component: CategoryRegisterComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
---
+++ src/app/pages/private/products/product-register/product-register.component.ts
@@ -0,0 +1,42 @@
+import { JsonPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-register',
+  imports: [ ReactiveFormsModule, JsonPipe ],
+  templateUrl: './product-register.component.html',
+  styleUrl: './product-register.component.css'
+})
+export class ProductRegisterComponent {
+  /** Atributos */
+  formData!: FormGroup;
+  categories: Array<{ _id: string; name: string; }> = [
+    { _id: '1', name: 'Drinks' },
+    { _id: '2', name: 'Soups' },
+    { _id: '3', name: 'Starters' },
+    { _id: '4', name: 'Cold dishes' }
+  ];
+
+  constructor() {
+    // Agrupacion de campos del formulario
+    this.formData = new FormGroup({
+      name: new FormControl( '' , [ Validators.required ] ),
+      description: new FormControl( '' ),
+      price: new FormControl( 0 , [ Validators.required, Validators.min( 0 ) ] ),
+      urlImage: new FormControl( '' ),
+      category: new FormControl( '', [ Validators.required ] )
+    });
+  }
+
+  onSubmit() {
+    // Obtiene los valores de los campos campos del formulario
+    const inputData = this.formData.value;
+
+    // Verifica el estado de validacion del formulario
+    if( this.formData.valid ) {
+      console.log( inputData );   // Enviar los datos al BackEnd
+    }
+
+  }
+}

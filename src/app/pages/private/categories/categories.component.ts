f02bb58d5f933b687144f52385904b69b9bdccee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2025-03-11 13:09:21 -0500

    [+] categories.component: Implementa Hook del ciclo de vida del componente ngOnInit() para vincular CategoryService getCategories() para obtener datos del BackEnd y renderizarlos en la vista en una tabla

--- src/app/pages/private/categories/categories.component.ts
+++ src/app/pages/private/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../../services/category.service';
+import { Category } from '../../../interfaces/category';
 
 @Component({
   selector: 'app-categories',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+  categories: Category[] = [];
+  isLoading: boolean = true;
 
+  constructor( private categoryService: CategoryService ) {}
+
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe({
+      next: ( data ) => {
+        console.log( data );
+        console.log( 'Successfully obtains categories' );
+
+        this.categories = data.data ?? [];    // Asignara una lista vacia para evitar asignar undefined
+      },
+      error: ( error ) => {
+        console.error( error );
+        this.isLoading = false;               // Asegura que el estado de carga se actualice en caso de error
+      },
+      complete: () => {
+        this.isLoading = false;               // También lo actualiza cuando la petición es exitosa
+      }
+    });
+  }
 }

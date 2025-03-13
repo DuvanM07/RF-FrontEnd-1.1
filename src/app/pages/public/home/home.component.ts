e5ab9e59bec673021f1b30096b3f154700afaaa7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2025-03-13 08:55:14 -0500

    [+] home.component.ts: ngOnInit() vinvula ProductService getProducts() para obtener lista de productos registrados

--- src/app/pages/public/home/home.component.ts
+++ src/app/pages/public/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../interfaces/product';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  products: Product[] = [];
+  isLoading: boolean = true;
 
+  constructor( private productService: ProductService ) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: ( data ) => {
+        console.log( data );
+        console.log( 'Successfully obtains products' );
+
+        this.products = data.data ?? [];    // Asignara una lista vacia para evitar asignar undefined
+      },
+      error: ( error ) => {
+        console.error( error );
+        this.isLoading = false;               // Asegura que el estado de carga se actualice en caso de error
+      },
+      complete: () => {
+        this.isLoading = false;               // También lo actualiza cuando la petición es exitosa
+      }
+    });
+  }
 }
